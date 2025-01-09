b2b3ee63bcd76bbc24df10f4f9411930acd709af
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-01-10 00:15:47 +0200

    roooooooooooom not work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './component/header/header';
 import Advertising from './component/Advertising/Advertising';
 import EndHome from './component/End Home/End Home';
 import ShoppingCart from './component/Shopping Cart/Shopping Cart';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 
 
 
@@ -16,8 +16,8 @@ function App() {
     {/* <Header/> */}
     {/* <Advertising /> */}
     {/* < EndHome /> */}
-    <ShoppingCart /> 
-    {/* <Home /> */}
+    {/* <ShoppingCart />  */}
+    <Home />
 
     </BrowserRouter>
   
---
+++ src/pages/Home/MouseMove.js
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react';
+// const Mouse = () => {
+//     const [move,setmove]=useState(false)
+    
+    
+//     return ( 
+//         ()=>{
+//             onMouseEnter={() => setMove(true)}
+//             onMouseLeave={() => setMove(false)}
+//         }
+
+//      );
+// }
+ 
+// export default Mouse;
