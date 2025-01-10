8c3d0812520168e91cd90bab50fef01272b33719
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-01-10 20:50:55 +0200

    set Routers Browser Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './component/header/header';
@@ -8,19 +8,39 @@ import ShoppingCart from './component/Shopping Cart/Shopping Cart';
 import Home from './pages/Home/Home';
 import ShoppingCartMain from './pages/Shopping Cart/Shopping Cart main';
 
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Home/>} />
+      {/* <Route path='/products' element={<Products/>} /> */}
+      {/* <Route path='' element={<Categories/>} /> */}
+      {/* <Route path='' element={<AboutUs/>} /> */}
+      {/* <Route path='' element={<Contact/>} /> */}
+      {/* <Route path='' element={<Blog/>} /> */}
+      <Route path='/ShoppingCartMain' element={<ShoppingCartMain/>} />
 
+
+
+
+
+
+
+
+    </Route>
+  )
+)
 
 function App() {
   return (
     <BrowserRouter>
 
-    {/* <Header/> */}
+    <Header/>
     {/* <Advertising /> */}
     {/* < EndHome /> */}
     {/* <ShoppingCart />  */}
 
     {/* <Home /> */}
-    <ShoppingCartMain/>
+    {/* <ShoppingCartMain/> */}
 
     </BrowserRouter>
   
