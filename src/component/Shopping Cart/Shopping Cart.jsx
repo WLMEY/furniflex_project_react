d9762a04767cea39ba11f50e7ad5b23330a89777
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-01-12 05:54:49 +0200

    add mobile sizeing in shopping cart and starter bage

--- src/component/Shopping Cart/Shopping Cart.jsx
+++ src/component/Shopping Cart/Shopping Cart.jsx
@@ -8,19 +8,20 @@ const ShoppingCart = () => {
 
     return (
 
-
+ 
 
         <div className="cart">
             <div className="product_price">
-                <div className="container">
+                <div className="container header_container">
                     <span>product</span>
                     <span>price</span>
                     <span>Quantiy</span>
                     <span>subtotal</span>
                 </div>
                 <div className="product_summary">
-                    <div className="summary container ">
-                        <div className="product-img"> <img src="/Materials/chair/barrel chair 8.jpg" alt="" />
+                    <div className="summary container container_list ">
+                        <div className="product_img"> 
+                            <img src="/Materials/chair/barrel chair 8.jpg" alt="" />
                             <span>Nova Nest Chair</span>
                         </div>
                         <span>$180.99</span>
@@ -35,8 +36,8 @@ const ShoppingCart = () => {
                         </div>
                     </div>
                     {/* <!-- icon  "x"  --> */}
-                    <div className="summary container ">
-                        <div className="product-img">
+                    <div className="summary container container_list ">
+                        <div className="product_img">
                             <img src="/Materials/chair/barrel chair 8.jpg" alt="" />
                             <span>Nova Nest Chair</span>
                         </div>
@@ -67,8 +68,8 @@ const ShoppingCart = () => {
                     <span>shipping</span>
                     <span>free</span>
                     <span className="all_column">Add coupon code <i className="fa-solid fa-arrow-right-long"></i> </span>
-                    <span>total</span>
-                    <span>$774.96</span>
+                    <span className='total'>total</span>
+                    <span className='total'>$774.96</span>
                     <button className="all_column btn_chekout">Proceed to Checkout</button>
                 </div>
             </div>
