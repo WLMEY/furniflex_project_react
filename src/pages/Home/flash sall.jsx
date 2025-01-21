e095760d3a7db229351636564fef63d3e416341e
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-01-21 04:28:08 +0200

    add some styles

--- src/pages/Home/flash sall.jsx
+++ src/pages/Home/flash sall.jsx
@@ -4,20 +4,41 @@ const FlashSale = () => {
     return (
         <div className="flashSale">
             <div className="fs1">
-                1
+                <div className="title">
+                    <h2>fleas sale!</h2>
+                    <p>Act fast to grab inceedible deals on select <br />
+                    furniture pieces in our limited-time flash sale.
+                    </p>
+                </div>
+                <div className="endtime">
+                    12h:36m:57s
+                </div>
+                <div className="image">
+                    <img src="" alt="" />
+                    <span>$599</span><span><del>$799</del></span>
+                </div>
+                <div className="ditilesitem">
+                        <h2>vintage leather armchai</h2>
+                        <p>bring a touch of retro charm to your home <br />
+                         with this vintage learher armchair. sturdy <br />
+                        constrcuction ensures durability
+                        </p>
+                        <button className="btn"> Shop Now<i class="fa-solid fa-arrow-right-long  "></i>
+                        </button>
+                </div>
             </div>
             <div className="fs2">
-                <img src="" alt="" />
+                <img src="../../../Materials/table/2.png" alt="" />
                 <div className="itemname">
                     <span>Rustic Coffee Table</span>
-                    <i class="fa-solid fa-arrow-right-long"></i>
+                    <i class="fa-solid fa-arrow-right-long arrowissingle "></i>
                 </div>
             </div>
             <div className="fs3">
-                <img src="" alt="" />
+                <img src="../../../Materials/bookShelf/4.png" alt="" />
                 <div className="itemname">
                     <span>Rustic Coffee Table</span>
-                    <i class="fa-solid fa-arrow-right-long"></i>
+                    <i class="fa-solid fa-arrow-right-long arrowissingle"></i>
                 </div>
             </div>
 
