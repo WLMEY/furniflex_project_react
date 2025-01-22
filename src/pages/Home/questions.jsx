b9431b17c4e9e4bcef0cea0fb014b75c5aa6a89a
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-01-22 04:54:12 +0200

    I'm done with the style in questions componant

--- src/pages/Home/questions.jsx
+++ src/pages/Home/questions.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 const Questions = () => {
-    return ( 
+    return (
 
         <div className="questions">
             <div className="head">
                 <span>got questions? we've <br />
-                got answers!
+                    got answers!
                 </span>
             </div>
             <div className="continer_questions">
-                <div className="question">
-                    <span className='que'>How do i chosse the right furniture for my space?</span>
+                <div className="question "> {/* show */}
+                    <div className="question_c">
+                        <span className='que'>How do i chosse the right furniture for my space?</span>
+                        <i class=" fa-solid fa-arrow-right-long" />
+                        {/* <i class=" fa-solid fa-xmark" /> */}
+
+                    </div>
                     <span className='ans'>
-                        
+                        Lorem ipsum dolor sit amet consectetur,
+                        adipisicing elit. Dolorum in id sit,
+                        error veritatis soluta labore. Asperiores,
+                        quibusdam corrupti? Sapiente!
                     </span>
+
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Questions;
