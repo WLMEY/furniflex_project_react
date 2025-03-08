7d65dc50d62f8278154039bb846a5e59fc8b13ea
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-03-08 02:56:30 +0200

    3 Am

--- src/pages/product details/Review.jsx
+++ src/pages/product details/Review.jsx
@@ -51,12 +51,22 @@ const Review = () => {
 
       <div className="CustomerSay">
         <p>Lorem ipsum dveniam error voluptatem laudantium. Quibusdam maiores facere beatae animi mollitia fugiat aliquam molestias nisi quo dolorum ad et nihil praesentium, asperiores impedit optio architecto. ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatum a hic culpa fugiat tenetur atque omnis consequuntur ratione? Tempore doloremque iure adipisci nobis doloribus beatae delectus quaerat odio eius? Mollitia nobis consectetur nisi qui quae, repellendus est eos? Earum repellendus esse distinctio pariatur deserunt. Omnis iure suscipit ab assumenda!</p>
-        
-      
-      
-      
-      
-      
+        <div className="filter">
+          <span className='colo'><i class="fa-regular fa-square-check"></i> Ease of assembly</span>
+          <span className='colo'><i class="fa-regular fa-square-check"></i> value</span>
+          <span className='colo'><i class="fa-regular fa-square-check"></i> Back support</span>
+          <span>comfort</span>
+          <span>Quality</span>
+          <span>Durability</span>
+          <span>fit</span>
+          <span className='colo2'><i class="fa-solid fa-minus"></i>Size</span>
+
+        </div>
+
+
+
+
+
       </div>
 
     </div>
