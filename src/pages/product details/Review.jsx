ec4d9073d7d0007f5d4ff5ed31867dd98d07d45d
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-03-08 02:45:46 +0200

    part 2

--- src/pages/product details/Review.jsx
+++ src/pages/product details/Review.jsx
@@ -1,14 +1,66 @@
 import React from 'react';
+import ShoppingCart from './../../component/Shopping Cart/Shopping Cart';
 
 
 const Review = () => {
-    return (
+  return (
 
-        <div className="reviews">
-          ولا اسمع صورت
-          
+    <div className="reviews">          {/* blocks  */}
+
+      <div className="customerReviews">
+        <div className="leftcontiner">
+          <span>Customer reviews</span>
+          <span className='stars'>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            4.7 out of 5
+          </span>
+          <span className='stars'>7,900 global ratings</span>
         </div>
-      );
+        <div className="rightcontiner">
+          <div className="star">
+            <span>5 Star </span>
+            <div className="tape"></div>
+            <span>95%</span>
+          </div>
+          <div className="star">
+            <span>5 Star </span>
+            <div className="tape"></div>
+            <span>95%</span>
+          </div>
+          <div className="star">
+            <span>5 Star </span>
+            <div className="tape"></div>
+            <span>95%</span>
+          </div>
+          <div className="star">
+            <span>5 Star </span>
+            <div className="tape"></div>
+            <span>95%</span>
+          </div>
+          <div className="star">
+            <span>5 Star </span>
+            <div className="tape"></div>
+            <span>95%</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="CustomerSay">
+        <p>Lorem ipsum dveniam error voluptatem laudantium. Quibusdam maiores facere beatae animi mollitia fugiat aliquam molestias nisi quo dolorum ad et nihil praesentium, asperiores impedit optio architecto. ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatum a hic culpa fugiat tenetur atque omnis consequuntur ratione? Tempore doloremque iure adipisci nobis doloribus beatae delectus quaerat odio eius? Mollitia nobis consectetur nisi qui quae, repellendus est eos? Earum repellendus esse distinctio pariatur deserunt. Omnis iure suscipit ab assumenda!</p>
+        
+      
+      
+      
+      
+      
+      </div>
+
+    </div>
+  );
 }
- 
+
 export default Review;
